7be60f4fe8f9611df11559d489a6c6246e07f654
Code Swimmer <codeswimmer@gmail.com>
2013-11-02 07:40:21 -0700

    Extract temporary functional tests out of cento
    These are just a few functions that were written to help with the
    feature development. Until a testing framework is in place, we'll
    use this.

--- cento/cento.js
+++ cento/cento.js
@@ -10,73 +10,7 @@ function Cento(canvas) {
 
 function initialize() {
   var canvasElem = document.getElementById('canvas');
-  globals.cento = new Cento(canvasElem)
+  globals.cento = new Cento(canvasElem);
 
-  testDogboneAddRemoveChildren(globals.cento);
-  testGameLoop(globals.cento);
+  //FunctionalTests.perform(globals.cento);
 }
-
-function testGraphics(that) {
-  var rect = {x:10, y:10, width:50, height:50};
-  var fillStyle = colorWithAlpha('#4682B4', 0.50);
-  that.dogbone.graphics.drawFilledRect(rect, fillStyle);
-
-  rect = {x:35, y:35, width:50, height:50};
-  fillStyle = colorWithAlpha('#82B446', 0.50);
-  that.dogbone.graphics.drawFilledRect(rect, fillStyle);
-}
-
-function testGameLoop(that) {
-  that.dogbone.start();
-
-  setTimeout(function() {
-    that.dogbone.stop();
-  }, 10000);
-}
-
-function testDogboneAddRemoveChildren(that) {
-  var frame = makeRectangle(100, 110, 120, 130);
-  var shape = Box.create(frame);
-  shape.backgroundColor = colorWithAlpha('#c70000', 0.5);
-  shape.zOrder = 1000;
-  that.dogbone.addChild(shape);
-
-  frame = makeRectangle(150, 175, 100, 100);
-  var box = Box.create(frame);
-  box.backgroundColor = colorWithAlpha('#00c700', 0.5);
-  box.zOrder = 999;
-  that.dogbone.addChild(box);
-
-  setTimeout(function() {
-    that.dogbone.removeChild(box);
-  }, 3000);
-}
-
-  /*
-  var counter = 0;
-  var maxTimes = 5;
-  var limit = maxTimes * 2;
-  var pulse = Pulse.create();
-
-  var firstHandler = function(payload) {
-    console.log('  firstHandler callback ' + counter);
-    if (counter++ >= maxTimes) {
-      console.log('firstHandler done');
-      pulse.off('pulse', firstHandler);
-    }
-  };
-
-  var secondHandler = function(payload) {
-    console.log('    secondHandler callback ' + counter);
-    if (counter++ >= limit) {
-      console.log('secondHandler done');
-      pulse.off('pulse', secondHandler);
-      pulse.stop();
-    }
-  };
-
-  pulse.on('pulse', firstHandler);
-  pulse.on('pulse', secondHandler);
-
-  pulse.start(500);
-  */
---
+++ cento/tests/functional/cento.functional.test.js
@@ -0,0 +1,83 @@
+
+function FunctionalTests() {
+  var that = {};
+
+  that.runTests = function(cento){
+    testDogboneAddRemoveChildren(cento);
+    testGameLoop(cento);
+  };
+
+  function testGraphics(cento) {
+    var rect = {x:10, y:10, width:50, height:50};
+    var fillStyle = colorWithAlpha('#4682B4', 0.50);
+    cento.dogbone.graphics.drawFilledRect(rect, fillStyle);
+
+    rect = {x:35, y:35, width:50, height:50};
+    fillStyle = colorWithAlpha('#82B446', 0.50);
+    cento.dogbone.graphics.drawFilledRect(rect, fillStyle);
+  }
+
+  function testGameLoop(cento) {
+    cento.dogbone.start();
+
+    var numSeconds = 10;
+    console.log('will stop GameLoop in ' + numSeconds + ' seconds');
+    setTimeout(function() {
+      cento.dogbone.stop();
+    }, numSeconds * 1000);
+  }
+
+  function testDogboneAddRemoveChildren(cento) {
+    var frame = makeRectangle(100, 110, 120, 130);
+    var shape = Box.create(frame);
+    shape.backgroundColor = colorWithAlpha('#c70000', 0.5);
+    shape.zOrder = 1000;
+    cento.dogbone.addChild(shape);
+
+    frame = makeRectangle(150, 175, 100, 100);
+    var box = Box.create(frame);
+    box.backgroundColor = colorWithAlpha('#00c700', 0.5);
+    box.zOrder = 999;
+    cento.dogbone.addChild(box);
+
+    var numSeconds = 3;
+    console.log('will remove green box in ' + numSeconds + ' seconds');
+    setTimeout(function() {
+      cento.dogbone.removeChild(box);
+      console.log('green box removed');
+    }, numSeconds * 1000);
+  }
+
+  return that;
+}
+
+FunctionalTests.create = function(){return new FunctionalTests();};
+FunctionalTests.perform = function(cento){FunctionalTests.create().runTests(cento);};
+  /*
+  var counter = 0;
+  var maxTimes = 5;
+  var limit = maxTimes * 2;
+  var pulse = Pulse.create();
+
+  var firstHandler = function(payload) {
+    console.log('  firstHandler callback ' + counter);
+    if (counter++ >= maxTimes) {
+      console.log('firstHandler done');
+      pulse.off('pulse', firstHandler);
+    }
+  };
+
+  var secondHandler = function(payload) {
+    console.log('    secondHandler callback ' + counter);
+    if (counter++ >= limit) {
+      console.log('secondHandler done');
+      pulse.off('pulse', secondHandler);
+      pulse.stop();
+    }
+  };
+
+  pulse.on('pulse', firstHandler);
+  pulse.on('pulse', secondHandler);
+
+  pulse.start(500);
+  */
