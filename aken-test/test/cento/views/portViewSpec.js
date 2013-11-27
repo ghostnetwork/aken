0f941e2e3fd827a4271bd81e0cc92c36121033f8
Code Swimmer <codeswimmer@gmail.com>
2013-11-27 14:36:37 -0800

    PortView LocalStorage

--- aken-test/test/cento/views/portViewSpec.js
+++ aken-test/test/cento/views/portViewSpec.js
@@ -2,6 +2,10 @@ var assert = require('assert');
 var should = require('should');
 var sinon = require('sinon');
 var util = require('util');
+var LocalStorage = require('node-localstorage').LocalStorage;
+var LSF = require('../../localStorageFixtures.js');
+var GF = require('../../dogbone/geometry/geometryFixtures.js');
+var PF = require('../kernel/ports/portFixtures.js');
 var PortView = require('../../../../cento/views/portView.js');
 require('../../../../verdoux/predicates.js');
 
@@ -13,4 +17,52 @@ describe('PortView', function(){
   beforeEach(function() {portView = PortView.create();});
 
   it('should be able to be created', function(){assert(existy(portView));});
+
+  describe('createFromJSON / createFromSpec', function(){
+    it('should be able to create a new object from the given JSON', function(){
+      var origPortView = createOriginalView();
+
+      var data = JSON.stringify(origPortView);
+      existy(data).should.be.true;
+
+      var clone = PortView.createFromJSON(data);
+      verifyClone(clone, origPortView);
+    });
+  });
+
+  describe('LocalStorage', function(){
+    it('should be able to save and restore from LocalStorage', function(done){
+      var origPortView = createOriginalView();
+
+      var data = JSON.stringify(origPortView);
+      existy(data).should.be.true;
+
+      var lsKey = "ViewSpec.LocalStorage";
+      LSF.global.initialize();
+      LSF.global.localStorage.setItem(lsKey, data);
+
+      var rawJSON = LSF.global.localStorage.getItem(lsKey);
+      existy(rawJSON).should.be.true;
+
+      var clone = PortView.createFromJSON(rawJSON);
+      verifyClone(clone, origPortView);
+
+      LSF.global.cleanup(function(error){
+        if (error) {throw error;}
+        done();
+      });
+
+    });
+  });
+
+  function createOriginalView() {
+    var origPortView = PortView.create(GF.Frame, PF.InputPort, null);
+    return origPortView;
+  }
+
+  function verifyClone(clone, origPortView) {
+    existy(clone).should.be.true;
+    clone.port.number.should.equal(origPortView.port.number);
+  }
+
 });
--- cento/views/portView.js
+++ cento/views/portView.js
@@ -3,7 +3,8 @@ kPortViewMoved  = 'PortView.Moved';
 function PortView(frame, port, actionView) {
   var that = CentoView.create(frame);
 
-  that.connectedTo = undefined;
+  Object.defineProperty(that, 'port', {get : function() {return _port;},enumerable : true});
+  Object.defineProperty(that, 'actionView', {get : function() {return _actionView;},enumerable : true});
 
   that.onMoved = function(delta) {
     var payload = {
@@ -33,13 +34,9 @@ function PortView(frame, port, actionView) {
   }
 
   configureSubscribers();
-
   
   var kPortAvailableColor = colorWithAlpha('#00ff00', 1.0);
   var kPortDefaultColor = colorWithAlpha('#ffffff', 1.0);
-
-  Object.defineProperty(that, 'port', {get : function() {return _port;},enumerable : true});
-  Object.defineProperty(that, 'actionView', {get : function() {return _actionView;},enumerable : true});
   
   var _port = port
     , _actionView = actionView;
@@ -49,10 +46,25 @@ function PortView(frame, port, actionView) {
 
 PortView.create = function(frame, port, actionView){return new PortView(frame, port, actionView);};
 
+PortView.createFromSpec = function(spec) {
+  var frame = Rectangle.createFromSpec(spec.frame);
+  var port = Port.createFromSpec(spec.port);
+  var actionView = null;//ActionView.createFromSpec(spec.actionView);
+  var portView = PortView.create(frame, port, actionView);
+  portView.shapeFromSpec(spec);
+  portView.viewFromSpec(spec);
+  return portView;
+};
+
+PortView.createFromJSON = function(portViewJSON) {return PortView.createFromSpec(JSON.parse(portViewJSON));};
+
 if (typeof module !== 'undefined') {
   module.exports = PortView;
   var util = require('util')
+    , Rectangle = require('../../dogbone/geometry/rectangle.js')
     , View = require('../../dogbone/views/view.js')
     , PubSub = require('../../verdoux/pubsub.js')
-    , CentoView = require('./centoView.js');
+    , Port = require('../kernel/ports/port.js')
+    , CentoView = require('./centoView.js')
+    , ActionView = require('./actionView.js');
 }
